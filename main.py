1b18a1327cab6b44b5123bea0e55d906b67da8c7
Kabelo Matlakala <Ebullienttheory30@gmail.com>
2025-04-24 22:14:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+from pet import Pet
+
+# Create a pet instance
+my_pet = Pet("Buddy")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.get_status()
+
+# Bonus features
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.show_tricks()
